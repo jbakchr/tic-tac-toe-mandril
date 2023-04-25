ed1d2c046fc56948b4dc41ccf812e2a55a8ed873
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-25 22:45:56 +0200

    Refactored homepage styles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,36 +2,49 @@ import { motion } from "framer-motion";
 
 import { MainLayout } from "../components/MainLayout";
 
+const mainStyle = {
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: "#0098c7",
+};
+
+const contentStyle = {
+  height: "150px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-around",
+  alignItems: "center",
+};
+
+const headerStyle = {
+  color: "white",
+  fontStyle: "italic",
+  fontFamily: "Lobster Two",
+  textShadow: "2px 2px black",
+  fontSize: "35px",
+  textAlign: "center",
+};
+
+const btnStyle = {
+  backgroundColor: "#ffd32a",
+  color: "#01a75b",
+  fontFamily: "Lobster Two",
+  padding: "10px 50px",
+  borderRadius: "50%",
+  fontSize: "20px",
+  boxShadow: "3px 3px black",
+  fontWeight: "bolder",
+};
+
 export const HomePage = () => {
   return (
-    <MainLayout
-      styles={{
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#0098c7",
-      }}
-    >
-      <div
-        style={{
-          height: "150px",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-around",
-          alignItems: "center",
-        }}
-      >
+    <MainLayout styles={mainStyle}>
+      <div style={contentStyle}>
         <motion.h1
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 3.0 }}
-          style={{
-            color: "white",
-            fontStyle: "italic",
-            fontFamily: "Lobster Two",
-            textShadow: "2px 2px black",
-            fontSize: "35px",
-            textAlign: "center",
-          }}
+          style={headerStyle}
         >
           Kryds og bol.. Mandril!
         </motion.h1>
@@ -39,16 +52,7 @@ export const HomePage = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 2.0 }}
-          style={{
-            backgroundColor: "#ffd32a",
-            color: "#01a75b",
-            fontFamily: "Lobster Two",
-            padding: "10px 50px",
-            borderRadius: "50%",
-            fontSize: "20px",
-            boxShadow: "3px 3px black",
-            fontWeight: "bolder",
-          }}
+          style={btnStyle}
         >
           Start spillet
         </motion.button>
