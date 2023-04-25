48bcf63d3f86fe4d3968460c423004f6eba98f44
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-25 21:28:05 +0200

    Added framer motion to h1 and button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 export const HomePage = () => {
   return (
     <div
@@ -10,16 +12,26 @@ export const HomePage = () => {
         backgroundColor: "#0098c7",
       }}
     >
-      <h1
+      <motion.h1
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 3.0 }}
         style={{
           color: "white",
           fontStyle: "italic",
           fontFamily: "Lobster Two",
+          textShadow: "2px 2px black",
         }}
       >
         Kryds og bol... Mandril!
-      </h1>
-      <button>Start spillet</button>
+      </motion.h1>
+      <motion.button
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 3.0 }}
+      >
+        Start spillet
+      </motion.button>
     </div>
   );
 };
