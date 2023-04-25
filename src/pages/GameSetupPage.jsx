4ed49f5617db3ab1b95769e8e345d2540b2840d2
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-26 00:53:30 +0200

    Restructed imports

--- src/pages/GameSetupPage.jsx
+++ src/pages/GameSetupPage.jsx
@@ -2,12 +2,12 @@ import { Container } from "@mui/material";
 
 import { MainLayout } from "../components/layout/MainLayout";
 import { Navbar } from "../components/navbar/Navbar";
+import { MainContent } from "../components/layout/MainContent";
 import { GameSetupHeader } from "../components/game/GameSetupHeader";
 import { Divider } from "../components/common/Divider";
-import { Arrow } from "../components/common/Arrow";
-import { AvatarImage } from "../components/game/AvatarImage";
-import { MainContent } from "../components/layout/MainContent";
 import { AvatarSelectionSection } from "../components/game/AvatarSelectionSection";
+import { AvatarImage } from "../components/game/AvatarImage";
+import { Arrow } from "../components/common/Arrow";
 
 export const GameSetupPage = () => {
   return (
