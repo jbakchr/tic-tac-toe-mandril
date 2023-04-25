79de5c47da995b2e5c51d7927f694bc2cd462da5
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-25 22:22:11 +0200

    Added styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,26 +12,48 @@ export const HomePage = () => {
         backgroundColor: "#0098c7",
       }}
     >
-      <motion.h1
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 3.0 }}
+      <div
         style={{
-          color: "white",
-          fontStyle: "italic",
-          fontFamily: "Lobster Two",
-          textShadow: "2px 2px black",
+          height: "150px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-around",
+          alignItems: "center",
         }}
       >
-        Kryds og bol... Mandril!
-      </motion.h1>
-      <motion.button
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 3.0 }}
-      >
-        Start spillet
-      </motion.button>
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 3.0 }}
+          style={{
+            color: "white",
+            fontStyle: "italic",
+            fontFamily: "Lobster Two",
+            textShadow: "2px 2px black",
+            fontSize: "35px",
+            textAlign: "center",
+          }}
+        >
+          Kryds og bol.. Mandril!
+        </motion.h1>
+        <motion.button
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 3.0 }}
+          style={{
+            backgroundColor: "#ffd32a",
+            color: "#01a75b",
+            fontFamily: "Lobster Two",
+            padding: "10px 50px",
+            borderRadius: "50%",
+            fontSize: "20px",
+            boxShadow: "3px 3px black",
+            fontWeight: "bolder",
+          }}
+        >
+          Start spillet
+        </motion.button>
+      </div>
     </div>
   );
 };
