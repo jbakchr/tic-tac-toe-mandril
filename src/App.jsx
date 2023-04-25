eeddbf7ebe5060b4c57f2e32601782e21365fff4
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-25 20:49:58 +0200

    Added initial routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import { RouterProvider } from "react-router";
+
+import { router } from "./routing/router";
+
 function App() {
-  return <div>Hello</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
