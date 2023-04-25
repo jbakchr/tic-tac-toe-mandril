90f1ff0503a3b44246266e05dfb1aa42c39f4b59
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-25 21:05:49 +0200

    Added initial component layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,19 @@
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        height: "100vh",
+        justifyContent: "center",
+        backgroundColor: "#0098c7",
+      }}
+    >
+      <h1 style={{ color: "white", fontStyle: "italic" }}>
+        Kryds og bol... Mandril!
+      </h1>
+      <button>Start spillet</button>
+    </div>
+  );
 };
