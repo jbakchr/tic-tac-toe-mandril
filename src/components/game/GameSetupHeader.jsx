3d91b0410d75731835d946ac57ded93e9904fd41
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-25 23:28:52 +0200

    Added game setup header

---
+++ src/components/game/GameSetupHeader.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const GameSetupHeader = () => {
+  return (
+    <h2
+      style={{
+        textAlign: "center",
+        margin: "20px",
+        fontFamily: "Lobster Two",
+        color: "grey",
+        fontSize: "30px",
+        letterSpacing: "3px",
+      }}
+    >
+      Vælg avatars
+    </h2>
+  );
+};
--- src/pages/GameSetupPage.jsx
+++ src/pages/GameSetupPage.jsx
@@ -1,24 +1,14 @@
 import React from "react";
 import { MainLayout } from "../components/MainLayout";
 import { Navbar } from "../components/navbar/Navbar";
+import { GameSetupHeader } from "../components/game/GameSetupHeader";
 
 export const GameSetupPage = () => {
   return (
     <MainLayout>
       <Navbar />
       <div style={{ flexGrow: 1 }}>
-        <h2
-          style={{
-            textAlign: "center",
-            margin: "20px",
-            fontFamily: "Lobster Two",
-            color: "grey",
-            fontSize: "30px",
-            letterSpacing: "3px",
-          }}
-        >
-          Vælg avatars
-        </h2>
+        <GameSetupHeader />
       </div>
     </MainLayout>
   );
