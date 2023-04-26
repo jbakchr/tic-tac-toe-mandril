61328d668ac4e2bf200070206e7fcf555484436d
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-26 09:09:09 +0200

    Added initial avatar

---
+++ src/components/game/Avatar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Avatar = ({ img }) => {
+  return (
+    <img
+      src="/images/broder_salsa.png"
+      alt="Broder Salsa"
+      style={{ width: "50px", height: "50px", borderRadius: "50%" }}
+    />
+  );
+};
--- src/components/game/AvatarImageList.jsx
+++ src/components/game/AvatarImageList.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { GameSetupSubHeader } from "./GameSetupSubHeader";
+import { Avatar } from "./Avatar";
 
 export const AvatarImageList = () => {
   return (
     <div>
       <GameSetupSubHeader />
+      <Avatar />
     </div>
   );
 };
