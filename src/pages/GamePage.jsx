3b169b8e345c47c99e1ca9f4699ae2da313aad9c
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-26 16:27:52 +0200

    Added initial components to 'GamePage'

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { MainLayout } from "../components/layout/MainLayout";
+import { Navbar } from "../components/navbar/Navbar";
+import { MainContent } from "../components/layout/MainContent";
+import { Container } from "@mui/material";
 
 export const GamePage = () => {
-  return <div>GamePage</div>;
+  return (
+    <MainLayout>
+      <Navbar />
+      <MainContent>
+        <Container maxWidth="xs">
+          <p>content here</p>
+        </Container>
+      </MainContent>
+    </MainLayout>
+  );
 };
