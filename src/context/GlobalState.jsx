a8bbcd1493fb900a587012a53eb5d3af999e6d29
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-26 11:34:25 +0200

    Added setting of 'playersTurn' when user clicks an avatar

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -7,6 +7,16 @@ export const GlobalState = ({ children }) => {
   const [playersTurn, setPlayersTurn] = useState(0);
 
   const addPlayer = (player) => {
+    /**
+     * When the user has clicked an avatar then we first swap the "playerTurn"
+     * and set that on state
+     */
+    const playerTurn = playersTurn === 0 ? 1 : 0;
+    setPlayersTurn(playerTurn);
+
+    /**
+     * And then we update the list of players
+     */
     setPlayers((prev) => [...prev, player]);
   };
 
