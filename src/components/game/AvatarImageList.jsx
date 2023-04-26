f8aa197d6d6477c3010ee776de9a4975e53683cd
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-26 10:40:49 +0200

    Added avatars list to state of 'AvatarImageList'

--- src/components/game/AvatarImageList.jsx
+++ src/components/game/AvatarImageList.jsx
@@ -1,14 +1,15 @@
+import { useState } from "react";
 import { Avatar } from "./Avatar";
 
-const avatars = [
-  { url: "bianca_landsvig.png", altText: "Bianca Landsvig" },
-  { url: "broder_salsa.png", altText: "Broder Salsa" },
-  { url: "david_hasselhof.png", altText: "David Hasselhof" },
-  { url: "kenneth.png", altText: "Kenneth" },
-  { url: "laughing_eagle.png", altText: "Laughing Eagle" },
-];
-
 export const AvatarImageList = () => {
+  const [avatars, setAvatars] = useState([
+    { url: "bianca_landsvig.png", altText: "Bianca Landsvig" },
+    { url: "broder_salsa.png", altText: "Broder Salsa" },
+    { url: "david_hasselhof.png", altText: "David Hasselhof" },
+    { url: "kenneth.png", altText: "Kenneth" },
+    { url: "laughing_eagle.png", altText: "Laughing Eagle" },
+  ]);
+
   const onAvatarClick = (index) => {
     // When a user clicks an avatar we first extract that avatar from the list of avatars
     const avatarSelected = avatars.splice(index, 1);
