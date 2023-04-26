314c57006abc1b553300b898dd28d50069309040
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-26 16:11:29 +0200

    Added showing of image in 'AvatarImagePlaceholder' when player prop is present

--- src/components/game/AvatarImagePlaceholder.jsx
+++ src/components/game/AvatarImagePlaceholder.jsx
@@ -1,17 +1,29 @@
-import React from "react";
+const imgStyle = {
+  border: "1px solid black",
+  width: "125px",
+  height: "125px",
+  borderRadius: "50%",
+};
 
 export const AvatarImagePlaceholder = ({ avatarImgText, player }) => {
   console.log(player);
+  if (player) {
+    return (
+      <img
+        src={`/images/${player.url}`}
+        alt={player.altText}
+        style={imgStyle}
+      />
+    );
+  }
+
   return (
     <div
       style={{
-        border: "1px solid black",
-        width: "125px",
-        height: "125px",
+        ...imgStyle,
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        borderRadius: "50%",
       }}
     >
       <p>{avatarImgText}</p>
